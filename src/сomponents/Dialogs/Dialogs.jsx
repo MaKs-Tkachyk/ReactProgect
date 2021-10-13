eb4b522fa6082b998aea9bf36fd0a812dfd7815f
MaKs-Tkachyk <tkachykmakc@gmail.com>
2021-10-13 22:44:29 +0300

    doing Dilogs components

--- src/сomponents/Dialogs/Dialogs.jsx
+++ src/сomponents/Dialogs/Dialogs.jsx
@@ -4,8 +4,24 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = () => {
     return (
-        <div className={s.Dialogs} >
-
+        <div className={s.dialogs} >
+            <div className={s.dialogs__dialogsItems}>
+                <div className={s.dialog__item}>
+                    Andrey
+                </div>
+                <div className={s.dialog__item}>
+                    Max
+                </div>
+                <div className={s.dialog__item}>
+                    Sergiy
+                </div>
+            </div>
+            <div className={s.dialogs__messages}>
+                <div className={s.dialogs__message}>Hi</div>
+                <div className={s.dialogs__message}>How are you</div>
+                <div className={s.dialogs__message}>I`m fine</div>
+                <div className={s.dialogs__message}>And you</div>
+            </div>
         </div>
     );
 }
